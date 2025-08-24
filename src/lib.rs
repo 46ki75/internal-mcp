c37b21c1fa9cee529a93ecb60068f9aaa4d90582
Ikuma Yamashita <me@ikuma.cloud>
2025-08-24 15:19:16 +0900

    feat: initialize project

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub(crate) mod error;
+pub(crate) mod handler;
+pub(crate) mod tool;
+
+pub async fn boot() -> Result<(), Box<dyn std::error::Error>> {
+    use rmcp::ServiceExt;
+
+    let handler = crate::handler::Handler::new()
+        .serve(rmcp::transport::stdio())
+        .await?;
+
+    handler.waiting().await?;
+
+    Ok(())
+}
